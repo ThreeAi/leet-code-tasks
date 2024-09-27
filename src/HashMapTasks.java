8a12816b174e80c0a27e190aed01811f1a7dea3a
ThreeAi <alexandrovaa2003@gmial.com>
2024-09-27 17:25:18 +0300

    *

--- src/HashMapTasks.java
+++ src/HashMapTasks.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HashMapTasks {
+import java.util.*;
+
+public class HashMapTasks {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for(String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if(!res.containsKey(key)) {
+                res.put(key, new ArrayList<String>());
+            }
+            res.get(key).add(str);
+        }
+        return res.values().stream().toList();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,10 @@ public class Main {
 
         char[][] height = new char[][]{{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
         int[][] matrix = new int[][]{{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
-        Solution.gameOfLife(matrix);
-        System.out.println(Arrays.deepToString(matrix));
+//        Solution.gameOfLife(matrix);
+        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(HashMapTasks.groupAnagrams(strs));
     }
 
 
