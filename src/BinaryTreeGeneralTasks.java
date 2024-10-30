7941c30b60d3053012be2a4a11ef32ca0529473a
ThreeAi <threeai@ThreeAis-MacBook-Pro.local>
2024-10-30 13:21:42 +0300

    146 104 100 226

---
+++ src/BinaryTreeGeneralTasks.java
@@ -0,0 +1,43 @@
+public class BinaryTreeGeneralTasks {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //104. Maximum Depth of Binary Tree
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    //100. Same Tree
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null && q != null || p != null && q == null || p != null && q != null && p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    //226. Invert Binary Tree
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        var temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
