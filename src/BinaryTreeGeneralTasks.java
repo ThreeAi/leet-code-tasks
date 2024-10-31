10400638de9d3da8e9990ae2b7b65f61132f49f6
ThreeAi <alexandrovaa2003@gmail.com>
2024-10-31 17:37:51 +0300

    101 105

--- src/BinaryTreeGeneralTasks.java
+++ src/BinaryTreeGeneralTasks.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class BinaryTreeGeneralTasks {
     public class TreeNode {
         int val;
@@ -41,4 +43,40 @@ public class BinaryTreeGeneralTasks {
         return root;
     }
 
+    //101. Symmetric Tree
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return nodeEquals(root.left, root.right);
+    }
+
+    private boolean nodeEquals(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null || p.val != q.val)
+            return false;
+        return nodeEquals(p.left, q.right) && nodeEquals(p.right, q.left);
+    }
+
+    //105. Construct Binary Tree from Preorder and Inorder Traversal
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return buildSubtree(inorder, 0, inorder.length - 1, preorder, 0, preorder.length - 1, map);
+    }
+
+    private TreeNode buildSubtree(int[] inorder, int inStart, int inEnd, int[] preorder, int preStart, int preEnd, HashMap<Integer, Integer> map) {
+        if (inStart > inEnd || preStart > preEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        int inRootIndex = map.get(root.val);
+        int leftTreeSize = inRootIndex - inStart;
+
+        root.left = buildSubtree(inorder, inStart, inRootIndex - 1, preorder, preStart + 1, preStart + leftTreeSize, map);
+        root.right = buildSubtree(inorder, inRootIndex + 1, inEnd, preorder, preStart + leftTreeSize + 1, preEnd, map);
+
+        return root;
+    }
 }
